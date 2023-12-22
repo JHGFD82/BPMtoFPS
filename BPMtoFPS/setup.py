6b2d728199c8cfe4f0e819bc6bc753291a473b76
Jeff Heller <jeffheller@jhgfd.com>
2023-12-22 18:17:08 -0500

    Small addition to verbiage in description.

--- BPMtoFPS/setup.py
+++ BPMtoFPS/setup.py
@@ -9,7 +9,8 @@ setup(
     description=("Convert time in a musical composition to time in a video production. "
                  "This package has tools to take either a specific beat or point in time and "
                  "converts it to a video timecode or number of frames based on the beats per "
-                 "minute of the song and the frames per second of the video."),
+                 "minute of the song and the frames per second of the video. Best suited for "
+                 "use with large datasets."),
     entry_points={
         'console_scripts': [
             'convert_audio_to_video_timing=BPMtoFPS.main:convert_audio_to_video_timing',
