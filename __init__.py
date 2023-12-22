ec08a038c22fd77bec2d7aedaea50764c2867d0b
Jeff Heller <jeffheller@jhgfd.com>
2023-12-21 22:17:42 -0500

    First drafts of package files.

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .main import convert_audio_to_video_timing
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BPMtoFPS",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Jeff Heller (JHGFD)",
+    author_email="jeffheller@jhgfd.com",
+    description=("Convert time in a musical composition to time in a video timeline. "
+                 "This package has tools to take either a specific beat or point in time and "
+                 "converts it to a video timecode or number of frames based on the beats per "
+                 "minute of the song and the frames per second of the video."),
+    entry_points={
+        'console_scripts': [
+            'convert_audio_to_video_timing=BPMtoFPS.main:convert_audio_to_video_timing',
+        ],
+    },
+    python_requires='>=3.6',
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False
+)
