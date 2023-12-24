d9122415a916cbb270cd8c1cc816155646bfdee6
Jeff Heller <jeffheller@jhgfd.com>
2023-12-24 14:23:41 -0500

    Added tests for new functions that round time values above .75.

--- tests/module_test.py
+++ tests/module_test.py
@@ -3,6 +3,7 @@ from BPMtoFPS.main import (
     ticks_to_seconds,
     beats_to_seconds,
     timecode_to_seconds,
+    calculate_frame_count,
     seconds_to_frames,
     seconds_to_timecode,
     convert_time
@@ -23,6 +24,10 @@ def test_timecode_to_seconds():
     assert timecode_to_seconds("1:00") == 60
 
 
+def test_calculate_frame_count():
+    assert calculate_frame_count(106, 29.97) == 3177
+
+
 def test_seconds_to_frames():
     # Example test case. Adjust according to your function's logic.
     assert seconds_to_frames(1, 30) == 30
@@ -33,6 +38,19 @@ def test_seconds_to_timecode():
     assert seconds_to_timecode(27.567, 29.97) == "27:16"
 
 
-def test_convert_audio_to_video_timing():
+def test_convert_time():
     # Add test cases for convert_audio_to_video_timing function
     assert convert_time('ticks', 'timecode', 240, 192, 29.97) == '0:04'
+
+
+def test_convert_time_beats_both():
+    assert convert_time('beats', 'both', 24, 192, 29.97) == (225, '7:15')
+
+
+def test_convert_time_ticks_both():
+    assert convert_time('ticks', 'both', 3840, 192, 29.97,
+                        ticks_per_beat=360) == (100, '3:10')
+
+
+def test_convert_timecode_ticks_both():
+    assert convert_time('timecode', 'both', '0:45.59', fps=29.97) == (1366, '45:17')
